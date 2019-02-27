7222d98c1841410de129c44f4d3a2a127c48c99b
polpoblu <47787556+polpoblu@users.noreply.github.com>
2019-02-27 22:21:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@
 
         // Animations Init
         new WOW().init();
+
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
