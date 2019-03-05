98c5f5ec0fa5232f823617bf745d268daa3d19e1
polpoblu <polpoblu@users.noreply.github.com>
2019-03-05 20:49:19 +0100

    Alpha

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+$(window).load(function() {
+    var $container = $('.animate-grid .gallary-thumbs');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    $('.animate-grid .categories a').click(function() {
+        $('.animate-grid .categories .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+});
